6ac96965f0230a836e31ac0658f25fa9e07a8423
Mattia Malacarne <malacarne.mattia97@gmail.com>
2021-05-09 15:02:53 +0200

    File upload funzionante, non si visualizzano

--- src/it/polimi/tiw/controllers/CreateAuction.java
+++ src/it/polimi/tiw/controllers/CreateAuction.java
@@ -1,6 +1,11 @@
 package it.polimi.tiw.controllers;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -9,11 +14,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import it.polimi.tiw.beans.Article;
 import it.polimi.tiw.core.DatabaseConnection;
@@ -26,10 +33,12 @@ import it.polimi.tiw.beans.User;
  * Servlet implementation class CreateAuction
  */
 @WebServlet("/CreateAuction")
+@MultipartConfig
 public class CreateAuction extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
+	private String imgFolder = "";
        
     public CreateAuction() {
         super();
@@ -37,6 +46,7 @@ public class CreateAuction extends HttpServlet {
 
     public void init() throws ServletException {
 		connection = DatabaseConnection.getConnection(getServletContext());
+		imgFolder = getServletContext().getInitParameter("imgFolder");
 	}
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
@@ -50,6 +60,8 @@ public class CreateAuction extends HttpServlet {
 			return;
 		}
 		
+		
+		
 		boolean badRequest = false;
 		
 		String nomeArticolo = null;
@@ -60,11 +72,41 @@ public class CreateAuction extends HttpServlet {
 		Integer rialzo_minAsta = null;
 		Timestamp scadenzaAsta = null;
 		
+		Part filePart = request.getPart("immagineArticolo");
+		if (filePart == null || filePart.getSize() <= 0) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing file in request!");
+			return;
+		}
+		
+		String contentType = filePart.getContentType();
+		
+		if (!contentType.startsWith("image")) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "File format not permitted");
+			return;
+		}
+		
+		String fileName = Paths.get(System.currentTimeMillis() + filePart.getSubmittedFileName()).getFileName().toString();
+		
+		String outPath = imgFolder + fileName;
+		
+		File file = new File(outPath);
+		
+		try (InputStream fileContent = filePart.getInputStream()) {
+			// TODO: WHAT HAPPENS IF A FILE WITH THE SAME NAME ALREADY EXISTS?
+			// you could override it, send an error or 
+			// rename it, for example, if I need to upload images to an album, and for each image I also save other data, I could save the image as {image_id}.jpg using the id of the db
+
+			Files.copy(fileContent, file.toPath());
+			Debugger.log("Immage upload OK");
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error while saving file");
+		}
 		
 		try {
 			nomeArticolo = request.getParameter("nomeArticolo");
 			descrizioneArticolo = request.getParameter("descrizioneArticolo");
-			immagineArticolo = request.getParameter("immagineArticolo");
+			//immagineArticolo = request.getParameter("immagineArticolo");
 			
 			prezzo_startAsta = Float.parseFloat(request.getParameter("prezzo_startAsta"));
 			rialzo_minAsta = Integer.parseInt(request.getParameter("rialzo_minAsta"));
@@ -73,6 +115,11 @@ public class CreateAuction extends HttpServlet {
 			Date date = (Date) sdf.parse(request.getParameter("scadenzaAsta"));
 									
 			scadenzaAsta = new Timestamp(date.getTime());
+			
+			
+            
+            immagineArticolo = URLEncoder.encode(fileName, "utf-8");
+            
 
 			//Controllo campi vuoti
 			badRequest = nomeArticolo.isEmpty() || descrizioneArticolo.isEmpty() || /*immagineArticolo.isEmpty() || */
