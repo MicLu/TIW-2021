4b6268a239daa8c29a21e736cf7e86f5c7cd6a1c
Michele Lucio <48596983+MicLu@users.noreply.github.com>
2021-06-28 21:45:50 +0200

    Aggiunti controlli condizionali dettaglio asta e listener search

--- WebContent/scripts/pagecontroller.js
+++ WebContent/scripts/pagecontroller.js
@@ -16,12 +16,18 @@
     }, false);
 
     var btn_vendo = document.getElementById("btn-vendo");
+    btn_vendo.addEventListener('mouseover', (e) => {
+        e.target.style.cursor = "pointer";
+    });
     btn_vendo.addEventListener("click", () => {
         pageOrchestrator.reset();
         nuovaAsta.show();
     });
 
     var btn_my_auc = document.getElementById("my-auc-btn");
+    btn_my_auc.addEventListener('mouseover', (e) => {
+        e.target.style.cursor = "pointer";
+    });
     btn_my_auc.addEventListener("click", () => {
         pageOrchestrator.reset();
         mieAsteAperte.show();
@@ -57,7 +63,34 @@
                     }
                 });
         }
-    })
+    });
+
+    //var btn_searc = document.getElementById("search-btn");
+    var btn_searc = document.getElementById("search-form").lastElementChild;
+    btn_searc.addEventListener("click", () => {
+        var search_form = document.getElementById("search-form");
+
+        makeCall("POST", "SearchJS", search_form,
+            function(req) {
+                if (req.readyState == XMLHttpRequest.DONE) {
+                    var message = req.responseText;
+                    if (req.status == 200) {
+                        var listaAste = JSON.parse(req.responseText);
+                        console.log(listaAste);
+                        if (listaAste.length == 0) {
+                            console.log("Lista vuota");
+                            self.alertContainer.textContent = "Nessuna asta trovata";
+                            return;
+                        }
+                        asteDisponibili.update(listaAste);
+                    }
+                } else {
+                    //FIXME: alertContainer non inizializzato
+                    self.alertContainer.textContent = message;
+                }
+            }
+        );
+    });
 
     function WelcomeMessage(username, messageContainer) {
         this.username = username;
@@ -116,6 +149,10 @@
                 var id = "auction_titolo" + element.idAsta;
                 var link_dettalio_asta = document.getElementById(id);
 
+                link_dettalio_asta.addEventListener('mouseover', (e) => {
+                    e.target.style.cursor = "pointer";
+                });
+
                 link_dettalio_asta.addEventListener("click", () => {
                     pageOrchestrator.reset();
                     dettaglioAsta.show(element.idAsta);
@@ -172,9 +209,23 @@
             console.log("MIE ASTE APERTE " + listaAste);
             listContainerBody.innerHTML = "";
             listaAste.forEach(element => {
-                //codice di creazione della tabella
+
+                //creazione della tabella
                 var row = createTableRow(element);
                 this.listContainerBody.append(row);
+
+                var id = "auction_titolo" + element.idAsta;
+                var link_dettalio_asta = document.getElementById(id);
+
+                link_dettalio_asta.addEventListener('mouseover', (e) => {
+                    e.target.style.cursor = "pointer";
+                });
+
+                link_dettalio_asta.addEventListener("click", () => {
+                    pageOrchestrator.reset();
+                    dettaglioAsta.show(element.idAsta);
+                });
+
             });
 
             document.getElementById("page-title").innerHTML = "<h1>Le mie aste</h1>";
@@ -184,8 +235,6 @@
 
     }
 
-    //Forse non serve perchè tanto la servlet GetMyAuctionJS restituisce già 
-    //due liste di mie aste aperte e mie aste chiuse
     function MieAsteChiuse(alertContainer, listContainer, listContainerBody) {
 
         this.alertContainer = alertContainer;
@@ -228,6 +277,19 @@
                 //codice di creazione della tabella
                 var row = createTableRow(element);
                 this.listContainerBody.append(row);
+
+                var id = "auction_titolo" + element.idAsta;
+                var link_dettalio_asta = document.getElementById(id);
+
+                link_dettalio_asta.addEventListener('mouseover', (e) => {
+                    e.target.style.cursor = "pointer";
+                });
+
+                link_dettalio_asta.addEventListener("click", () => {
+                    pageOrchestrator.reset();
+                    dettaglioAsta.show(element.idAsta);
+                });
+
             });
 
             //rendo visibile la tabella alla fine della creazione
@@ -240,7 +302,7 @@
 
         this.alertContainer = alertContainer;
         this.detContainer = detContainer;
-        //TODO
+
         this.reset = function() {
             //Nascondo l'intera tabella
             this.detContainer.style.display = "none";
@@ -254,8 +316,6 @@
                         var message = req.responseText;
                         if (req.status == 200) {
                             var asta = JSON.parse(req.responseText);
-                            //Asta da visualizzare
-                            asta = asta[0];
                             if (asta.length == 0) {
                                 self.alertContainer.textContent = "Nessuna asta disponibile";
                                 return;
@@ -270,27 +330,72 @@
         }
 
         this.update = function(product) {
-            console.log(product);
+            console.log(product[0]);
 
-            document.getElementById("auc-own-name").innerHTML = product.proprietario;
-            document.getElementById("auc-exp").innerHTML = product.scadenza;
-            document.getElementById("auc-desc").innerHTML = product.article.descrizione;
-            document.getElementById("auc-curr").innerHTML = product.prezzo_start;
-            document.getElementById("min-offer").innerHTML = product.rialzo_min;
-            document.getElementById("auc-exp").innerHTML = product.scadenza;
+            var auction = product[0];
+            var offerList = product[2];
+            var min_offer = product[5];
 
-            var imgReqUrl = "http://localhost:8080/TIW-2021/GetImage?image=" + product.article.immagine;
+            document.getElementById("prod-name").innerHTML = auction.article.nome;
+            document.getElementById("auc-own-name").innerHTML = auction.proprietario;
+            document.getElementById("auc-exp").innerHTML = auction.scadenza;
+            document.getElementById("auc-desc").innerHTML = auction.article.descrizione;
+            document.getElementById("auc-curr").innerHTML = auction.prezzo_start;
+            document.getElementById("min-offer").innerHTML = min_offer;
+
+            //document.getElementById("auc-btn").href = "/CloseAuctionJS?auctionId=" + auction.idAsta;
+            document.getElementById("auc-btn").addEventListener("click", (e) => {
+                makeCall("POST", "CloseAuctionJS", auction.idAsta,
+                    function(req) {
+                        if (req.readyState == XMLHttpRequest.DONE) {
+                            var message = req.responseText;
+                            if (req.status == 200) {
+                                var listaAste = JSON.parse(req.responseText);
+                                console.log(listaAste);
+                                if (listaAste.length == 0) {
+                                    console.log("Lista vuota");
+                                    self.alertContainer.textContent = "Nessuna asta disponibile";
+                                    return;
+                                }
+                                self.update(listaAste);
+                            }
+                        } else {
+                            self.alertContainer.textContent = message;
+                        }
+                    }
+                );
+            })
+
+            var imgReqUrl = "http://localhost:8080/TIW-2021/GetImage?image=" + auction.article.immagine;
 
             var img = document.createElement("img");
             img.src = imgReqUrl;
             img.classList.add("product-image");
-
             document.getElementById("prod-img").append(img);
-            if (product.proprietario != sessionStorage.getItem("username"))
-            {
+
+
+            if (!(auction.proprietario === sessionStorage.getItem("username") &&
+                    auction.auctionStatus === 'SCADUTA')) {
                 document.getElementById("owner-zone").style.display = "none";
             }
-            
+            if (auction.auctionStatus !== 'CHIUSA') {
+                document.getElementById("auc-result").style.display = "none";
+            }
+
+            if (auction.auctionStatus === 'CHIUSA') {
+                if (offerList == undefined) {
+                    //Non ci sono offerte
+                    document.getElementById("auc-result").innerHTML = "Asta scaduta senza offerte";
+                } else {
+                    var winner = product[6];
+                    document.getElementById("auc-winner-name").innerHTML = winner.nome + " " + winner.cognome;;
+
+                    document.getElementById("auc-final-price").innerHTML = auction.prezzo_start;
+                    document.getElementById("auc-winner-address").innerHTML = winner.indirizzo;
+                }
+            }
+
+
             detContainer.style.display = "block";
         }
 
